bd441e80d7f5266e9e175910f4ca8aae8e7ce650
IsmaelVen <joseismaelventura631@gmail.com>
2025-07-31 11:50:00 -0600

    Ejemplo

---
+++ EjemploApp/Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace EjemploGitApp
+{
+    public class Usuario
+    {
+        // Propiedades
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
